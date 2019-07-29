cc7b9b78253f324ae3a5720a38a394e3c900e8e9
Sebastian Landwehr <info@dword-design.de>
2019-07-29 05:34:18 +0200

    a

---
+++ src/formatter/depcruise.js
@@ -0,0 +1,13 @@
+import { cruise } from 'dependency-cruiser'
+import { findConfig, gitignore, aliases } from '@dword-design/base'
+import { keys } from '@functions'
+import pkgDir from 'pkg-dir'
+import path from 'path'
+
+export default () => Promise.all([pkgDir(), findConfig()])
+  .then(([workspacePath, { depgraphIgnores }]) => {
+
+    const ignores = [...gitignore, ...aliases |> keys, ...depgraphIgnores]
+
+    return cruise([path.resolve(workspacePath, 'src')], { exclude: `(${ignores.join('|')})` })
+  })
--- src/formatter/spring.js
+++ src/formatter/spring.js
@@ -1,27 +1,16 @@
 import express from 'express'
 import path from 'path'
-import { cruise } from 'dependency-cruiser'
-import { findConfig, gitignore, aliases } from '@dword-design/base'
-import { keys, map, flatMap } from '@functions'
+import { map, flatMap } from '@functions'
+import depcruise from './depcruise'
 
 export const prepareExpress = app => app
   .use(express.static(path.resolve(__dirname, 'client')))
   .use('/graph', express.Router()
-    .get('/', (req, res) => findConfig()
-      .then(({ depgraphIgnores }) => {
-        const ignores = [
-          ...gitignore,
-          ...aliases |> keys,
-          ...depgraphIgnores,
-        ]
-
-        const modules = cruise([process.cwd()], { exclude: `(${ignores.join('|')})` }).modules
-
-        return res.send({
-          modules: modules |> map('source'),
-          dependencies: modules
-            |> flatMap(({ source, dependencies }) => dependencies |> map(({ resolved }) => ({ source, target: resolved }))),
-        })
-      })
+    .get('/', (req, res) => depcruise()
+      .then(({ modules }) => res.send({
+        modules: modules |> map('source'),
+        dependencies: modules
+          |> flatMap(({ source, dependencies }) => dependencies |> map(({ resolved }) => ({ source, target: resolved }))),
+      }))
     )
   )
