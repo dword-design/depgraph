fba7a321b79bf6e41a5974461ab7df0f38080ca1
Sebastian Landwehr <landwehr@zebralog.de>
2019-07-29 11:32:45 +0200

    adjust cli

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,7 @@ import program from 'commander'
 import depgraph from '.'
 
 program
-  .option('-t, --format <name>', 'The formatter to use')
+  .arguments('[formatter]')
+  .action(depgraph)
 
 program.parse(process.argv)
-
-const formatterName = program.format
-
-depgraph(formatterName)
