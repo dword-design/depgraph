2f39550d8b1b7d4da3efd3dc1d7211a85882135e
Sebastian Landwehr <info@dword-design.de>
2020-01-13 23:33:38 +0100

    only output workspace dependencies in workspace root

--- packages/server/src/depcruise.js
+++ packages/server/src/depcruise.js
@@ -1,33 +1,46 @@
 import { spawn } from 'child-process-promise'
-import { mapValues, property, map, pickBy, replace } from '@dword-design/functions'
+import { mapValues, property, map, pickBy, replace, values, some } from '@dword-design/functions'
+import getWorkspaces from 'get-workspaces'
 
 export default async () => {
-  const { stdout } = await spawn(
-    'depcruise',
-    [
-      '--exclude', /(^|\\|\/)node_modules(\\|\/)/.source,
-      '--include-only', /(^|\\|\/)(src|dist)(\\|\/)/.source,
-      '--do-not-follow', /(^|\\|\/)dist(\\|\/)/.source,
-      '--output-type', 'json',
-      '.',
-    ],
-    { capture: ['stdout'] }
-  )
-  const cleanPath = path => path
-    |> replace(/^packages(\\|\/)/, '')
-    |> replace(/(^|\\|\/)(src|dist)(\\|\/)/, '$1')
+  const workspaces = await getWorkspaces()
+  if (workspaces !== null && workspaces.length > 0) {
+    return workspaces
+      |> map(({ name, config: { dependencies = {} } }) => ({
+        source: name,
+        dependencies: dependencies
+          |> pickBy((version, name) => workspaces |> some({ name }))
+          |> mapValues((version, name) => ({ resolved: name }))
+          |> values,
+      }))
+  } else {
+    const { stdout } = await spawn(
+      'depcruise',
+      [
+        '--exclude', /(^|\\|\/)node_modules(\\|\/)/.source,
+        '--include-only', /(^|\\|\/)(src|dist)(\\|\/)/.source,
+        '--do-not-follow', /(^|\\|\/)dist(\\|\/)/.source,
+        '--output-type', 'json',
+        '.',
+      ],
+      { capture: ['stdout'] }
+    )
+    const cleanPath = path => path
+      |> replace(/^packages(\\|\/)/, '')
+      |> replace(/(^|\\|\/)(src|dist)(\\|\/)/, '$1')
 
-  return stdout
-    |> JSON.parse
-    |> property('modules')
-    |> pickBy(({ source }) => !/(^|\\|\/)dist(\\|\/)/.test(source))
-    |> mapValues(module => ({
-      ...module,
-      source: module.source |> cleanPath,
-      dependencies: module.dependencies
-        |> map(dependency => ({
-          ...dependency,
-          resolved: dependency.resolved |> cleanPath,
-        })),
-    }))
+    return stdout
+      |> JSON.parse
+      |> property('modules')
+      |> pickBy(({ source }) => !/(^|\\|\/)dist(\\|\/)/.test(source))
+      |> mapValues(module => ({
+        ...module,
+        source: module.source |> cleanPath,
+        dependencies: module.dependencies
+          |> map(dependency => ({
+            ...dependency,
+            resolved: dependency.resolved |> cleanPath,
+          })),
+      }))
+  }
 }
