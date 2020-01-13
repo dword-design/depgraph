568ff4655799c34350d0210ad03ef3c4399e4c66
Sebastian Landwehr <info@dword-design.de>
2020-01-13 22:10:45 +0100

    workspaces support

--- packages/server/src/depcruise.js
+++ packages/server/src/depcruise.js
@@ -1,6 +1,33 @@
 import { spawn } from 'child-process-promise'
+import { mapValues, property, map, pickBy, replace } from '@dword-design/functions'
 
 export default async () => {
-  const { stdout } = await spawn('depcruise', ['--exclude', /(^|\\|\/)node_modules[\\/]/.source, '--output-type', 'json', 'src'], { capture: ['stdout'] })
-  return JSON.parse(stdout).modules
+  const { stdout } = await spawn(
+    'depcruise',
+    [
+      '--exclude', /(^|\\|\/)node_modules(\\|\/)/.source,
+      '--include-only', /(^|\\|\/)(src|dist)(\\|\/)/.source,
+      '--do-not-follow', /(^|\\|\/)dist(\\|\/)/.source,
+      '--output-type', 'json',
+      '.',
+    ],
+    { capture: ['stdout'] }
+  )
+  const cleanPath = path => path
+    |> replace(/^packages(\\|\/)/, '')
+    |> replace(/(^|\\|\/)(src|dist)(\\|\/)/, '$1')
+
+  return stdout
+    |> JSON.parse
+    |> property('modules')
+    |> pickBy(({ source }) => !/(^|\\|\/)dist(\\|\/)/.test(source))
+    |> mapValues(module => ({
+      ...module,
+      source: module.source |> cleanPath,
+      dependencies: module.dependencies
+        |> map(dependency => ({
+          ...dependency,
+          resolved: dependency.resolved |> cleanPath,
+        })),
+    }))
 }
