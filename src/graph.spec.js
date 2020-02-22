17a34b5edb9e25e6cf22c09c5fa133c3b280a48a
dword <sebastianlandw@gmail.com>
2020-02-22 11:34:44 +0000

    fix: make tests more efficient

--- src/graph.spec.js
+++ src/graph.spec.js
@@ -3,9 +3,10 @@ import stealthyRequire from 'stealthy-require'
 import outputFiles from 'output-files'
 import kill from 'tree-kill'
 import portReady from 'port-ready'
-import puppeteer from '@dword-design/puppeteer'
 import execa from 'execa'
 import P from 'path'
+import axios from 'axios'
+import { property } from '@dword-design/functions'
 
 export default {
   valid: () => withLocalTmpDir(async () => {
@@ -16,18 +17,8 @@ export default {
     await execa('nuxt', ['build', '--config-file', nuxtConfigFilename], { cwd: P.resolve(__dirname, '..') })
     const graph = stealthyRequire(require.cache, () => require('./graph'))
     const childProcess = graph({ log: true })
-      .catch(error => {
-        if (error.code !== null) {
-          throw error
-        }
-      })
-      .childProcess
     await portReady(3000)
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('http://localhost:3000')
-    expect(await page.content()).toMatch('Depgraph')
-    await browser.close()
+    expect(axios.get('http://localhost:3000') |> await |> property('data')).toMatch('Depgraph')
     kill(childProcess.pid)
   }),
 }
